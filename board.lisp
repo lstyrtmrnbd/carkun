7fa1dba9506740f586a93efa20431003f36858d3
Marisa Kirisame <soymilk@airmail.cc>
2018-03-11 15:00:55 -0500

    Preliminary modelling

--- board.lisp
+++ board.lisp
@@ -3,7 +3,7 @@
 (defvar colors '(red blue purple yellow green trash))
 
 (defclass puyo ()
-  (color))
+  ((color :accessor col :initarg :col)))
 
 (defclass state ()
   ((array :accessor arr)
@@ -14,17 +14,33 @@
           :initarg :w :type integer)
    (height :accessor h :initform 12
            :initarg :h :type integer)
-   (state :accessor state
-          :initarg :state)
-   (resolved :accessor res :initform t)))
+   (states :accessor states
+          :initarg :states)))
 
-(defvar state (make-array '() :element-type puyo))
+(defvar state (make-array '() :element-type 'puyo :initial-element nil))
+
+;;; Puyo Ops
+(defgeneric is-trash (puyo))
+
+(defmethod is-trash ((p puyo))
+  (eq (col p) 'trash))
+
+;;; State Ops
+(defgeneric get-dimensions (object)) ;; board or state dimensions
+
+(defmethod get-dimensions ((s state))
+  (array-dimensions (arr s)))
+
+(defgeneric get-puyo)      ;; gets a puyo at an x,y for a board/state?
+(defgeneric is-resolved)   ;; checks for outstanding chains in a state
+
+;;; Board Ops
+(defmethod get-dimensions ((b board))
+  (list (w b) (h b)))
 
 (defgeneric state-count)   ;; amount of states in a board
 (defgeneric add-state)     ;; push a state to the board state stack
-(defgeneric get-dimensions);; board or state dimensions
-(defgeneric get-puyo)      ;; gets a puyo at an x,y
-(defgeneric is-resolved)   ;; checks for outstanding chains
+(defgeneric project-states);; give next puyo drop and project resultant states
 
 (defun new-board ((w 6) (h 12))
   (make-instance board :w w :h h))
