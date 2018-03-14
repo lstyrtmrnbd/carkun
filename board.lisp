5290389a63b4cb1bacffcce8149d1ad0116532cb
Marisa Kirisame <soymilk@airmail.cc>
2018-03-14 16:13:18 -0500

    print-state and settledp

--- board.lisp
+++ board.lisp
@@ -3,10 +3,12 @@
 (defvar colors '(red blue purple yellow green trash))
 
 (defclass puyo ()
-  ((color :accessor col :initarg :col)))
+  ((color :accessor color :initarg :color)))
 
 (defclass state ()
-  ((array :accessor arr)
+  ((width :accessor w :initarg :w :type integer)
+   (height :accessor h :initarg :h :type integer)
+   (array :accessor arr :initarg :arr)
    (resolved :accessor res :initform t)))
 
 (defclass board ()
@@ -14,11 +16,17 @@
           :initarg :w :type integer)
    (height :accessor h :initform 12
            :initarg :h :type integer)
-   (states :accessor states
+   (states :accessor states :initform '()
            :initarg :states)))
 
 (defun make-puyo (color)
-  (make-instance 'puyo :col color))
+  (make-instance 'puyo :color color))
+
+(defun make-state (h w)
+  (make-instance 'state :h h :w w :arr (make-array (list h w) :initial-element nil)))
+
+(defun make-board ()
+  (make-instance 'board))
 
 (defvar state (make-array '(12 6) :element-type 'puyo :initial-element nil))
 
@@ -39,15 +47,51 @@
 (defmethod get-puyo ((s state) (x integer) (y integer))
   (aref (arr s) x y))
 
-(defgeneric is-resolved)   ;; checks for outstanding chains in a state
+(defun set-puyo (s p x y)
+  (setf (aref (arr s) x y) p))
+
+(defgeneric is-resolved (state)) ;; checks for outstanding chains in a state
 
 ;;; Board Ops
 (defmethod get-dimensions ((b board))
   (list (h b) (w b)))
 
-(defgeneric state-count)   ;; amount of states in a board
-(defgeneric add-state)     ;; push a state to the board state stack
-(defgeneric project-states);; give next puyo drop and project resultant states
+(defun state-count (board)
+  (length (states board))) ;; amount of states in a board
 
-(defun new-board ((w 6) (h 12))
-  (make-instance board :w w :h h))
+(defun push-state (state board)
+  (when (not (equal (get-dimensions state)
+                    (list (h board) (w board))))
+    (error "Board and state dimension mismatch."))
+  (push state (states board)))
+
+(defun print-state (b index)
+  (let  ((state (nth index (states b))))
+    (dotimes (y (1- (h state)))
+      (dotimes (x (1- (w state)))
+        (let ((p (aref (arr state) y x)))
+          (cond ((null p) (format t " . "))
+                ((eq (color p) 'purple) (format t " p "))
+                ((eq (color p) 'red) (format t " r "))
+                ((eq (color p) 'green) (format t " g "))
+                ((eq (color p) 'blue) (format t " b "))
+                ((eq (color p) 'yellow) (format t " y "))
+                ((eq (color p) 'trash) (format t " x ")))))
+      (format t "~%"))))
+    
+(defgeneric add-state ()) ;; push a state to the board state stack
+
+(defgeneric project-states ()) ;; give next puyo drop and project resultant states
+
+(defun resolvedp ()
+  "Check if board is resolved: no ")
+
+(defun settledp (state)
+  "Check that no pieces have empty space below them."
+  (destructuring-bind (h w) (array-dimensions (arr state))
+    (loop for y from 1 below h do
+      (loop for x from 0 below w do
+           (when (and (not (null (aref (arr state) y x)))
+                      (null (aref (arr state) (1- y) x)))
+             (return-from settledp nil)))))
+  t)
